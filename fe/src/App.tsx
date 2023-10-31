a19ab49cb7eb6c2b3d30243d807954612c8545f6
seo-seoungjun <tjtjdwns1860@naver.com>
2023-11-01 07:13:01 +0900

    initial:front-settings

---
+++ fe/src/App.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
+
+function App() {
+  return <div>main 페이지</div>;
+}
+
+export default App;
---
+++ fe/src/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { ThemeProvider } from 'styled-components';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+
+const darkTheme = {
+  textColor: 'white',
+};
+
+root.render(
+  <React.StrictMode>
+    <ThemeProvider theme={darkTheme}>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
+);
