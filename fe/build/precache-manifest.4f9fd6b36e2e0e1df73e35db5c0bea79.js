23fa6c0207c54bd6f9964d550727fa4532786155
seo-seoungjun <tjtjdwns1860@naver.com>
2024-05-09 14:00:39 +0900

    BUILD:add build file

---
+++ fe/build/precache-manifest.4f9fd6b36e2e0e1df73e35db5c0bea79.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddfcce4093cda1877c1f78af9443b175",
+    "url": "/index.html"
+  },
+  {
+    "revision": "13d88b154f0e8f4e9288",
+    "url": "/static/js/2.c6b4e1b5.chunk.js"
+  },
+  {
+    "revision": "95c2f1c08fb31024f1cc",
+    "url": "/static/js/main.6aed7d70.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
